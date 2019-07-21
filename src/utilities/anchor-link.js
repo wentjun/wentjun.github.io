4d5ef3372c7c33c1e760808fe65cdae1cc1ce6d0
Wen Tjun <wentjun289@hotmail.com>
2019-07-21 12:49:12 +0800

    smooth scrolling between sections
    - created component for anchor links to various sections
    - installed smoothscroll-polyfill to allow smooth scroll behaviour in
    Safari and IE

---
+++ src/utilities/anchor-link.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class AnchorLink extends React.Component {
+  constructor(props) {
+    super(props);
+    this.jumpTo = this.jumpTo.bind(this);
+  }
+
+  jumpTo() {
+    const targetElement = document.getElementById(this.props.location);
+    targetElement.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  render() {
+    return (
+      <a onClick={this.jumpTo} className="downArrow" aria-label="down arrow">
+        <i></i>
+      </a>
+    );
+  }
+}
+
+export default AnchorLink;
